6e4aa89f2211a8828c345b971a4417e5ab2c2724
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2016-08-22 20:20:46 -0300

    substitui nos textos espaços por lojas

--- Theme.php
+++ Theme.php
@@ -159,24 +159,40 @@ class Theme extends BaseV1\Theme{
         return [
                 'entities: Spaces' => 'Lojas',
                 'site: name' => $name,
-//            'site: description' => App::i()->config['app.siteDescription'],
-//            'site: in the region' => 'na região',
-//            'site: of the region' => 'da região',
-//            'site: owner' => 'Secretaria',
-//            'site: by the site owner' => 'pela Secretaria',
-//
-//            'home: title' => "Bem-vind@!",
+
                 'home: abbreviation' => "DL",
                 'home: colabore' => "Colabore com o Desenvolvimento Local",
                 'home: welcome' => "O Mapas do Desenvolvimento Local de São Paulo plataforma livre, gratuita e colaborativa de mapeamento dos pequenos empreendimentos do território de São Paulo.",
                 'home: events' => "Você pode pesquisar eventosnos campos de busca combinada. Como usuário cadastrado, você pode incluir seus eventos na plataforma e divulgá-los gratuitamente.",
                 'home: agents' => "Você pode colaborar na gestãoo desenvolvimento local com suas próprias informações, preenchendo seu perfil de agente de desenvolvimento local. Neste espaço, estão registradoagentes da Asampa, liderançs locais e empreendedores; uma rede de atores envolvidos na cena econômica da região. Você pode cadastrar um ou mais agentes (grupos, coletivos, empresas, etc.), além de associar ao seu perfil eventos e espaço com divulgação gratuita.",
                 'home: spaces' => "Procure por espaços incluídos na plataforma, acessando os campos de busca combinada que ajudam na precisão de sua pesquisa. Cadastre também os espaços onde desenvolve suas atividades.",
-//            'home: projects' => "Reúne projetos culturais ou agrupa eventos de todos os tipos. Neste espaço, você encontra leis de fomento, mostras, convocatórias e editais criados, além de diversas iniciativas cadastradas pelos usuários da plataforma. Cadastre-se e divulgue seus projetos.",
-//            'home: home_devs' => 'Existem algumas maneiras de desenvolvedores interagirem com o Mapas Culturais. A primeira é através da nossa <a href="https://github.com/hacklabr/mapasculturais/blob/master/doc/api.md" target="_blank">API</a>. Com ela você pode acessar os dados públicos no nosso banco de dados e utilizá-los para desenvolver aplicações externas. Além disso, o Mapas Culturais é construído a partir do sofware livre <a href="http://institutotim.org.br/project/mapas-culturais/" target="_blank">Mapas Culturais</a>, criado em parceria com o <a href="http://institutotim.org.br" target="_blank">Instituto TIM</a>, e você pode contribuir para o seu desenvolvimento através do <a href="https://github.com/hacklabr/mapasculturais/" target="_blank">GitHub</a>.',
-//
-//            'search: verified results' => 'Resultados Verificados',
-//            'search: verified' => "Verificados"
+            
+            
+                'entities: Spaces of the agent'=> 'Lojas do agente',
+                'entities: Space Description'=> 'Descrição da Loja',
+                
+                'entities: My Spaces'=> 'Minhas Lojas',
+                'entities: My spaces'=> 'Minhas lojas',
+
+                'entities: no registered spaces'=> 'nenhuma loja cadastrada',
+                'entities: no spaces'=> 'nenhuma loja',
+
+                'entities: Space' => 'Loja',
+                'entities: Spaces' => 'Lojas',
+                'entities: space' => 'loja',
+                'entities: spaces' => 'lojas',
+                'entities: parent space' => 'loja pai',
+                'entities: a space' => 'uma loja',
+                'entities: the space' => 'a loja',
+                'entities: of the space' => 'da loja',
+                'entities: In this space' => 'Nesta loja',
+                'entities: in this space' => 'nesta loja',
+                'entities: registered spaces' => 'lojas cadastradas',
+                'entities: new space' => 'nova loja',
+
+                'entities: space found' => 'loja encontrada',
+                'entities: spaces found' => 'lojas encontradas',
+
         ];
     }
 
@@ -218,6 +234,11 @@ class Theme extends BaseV1\Theme{
             
             $app->hook('API.find(space).params', function(&$params) use($subdomain){
                 $params['regiao'] = 'EQ(' . self::$subdomains[$subdomain]['name'] . ')';
+            });
+            
+            $app->hook('entity(Space).insert:before', function() use ($subdomain) {
+                $cfg = self::$subdomains[$subdomain];
+                $this->regiao = $cfg['name'];
             });
         }
         
